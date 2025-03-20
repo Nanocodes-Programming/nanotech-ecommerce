8e7ec4fbc5a8c7f9292fb4a5ac687458285d5fd2
harri <harrigold.18@gmail.com>
2025-03-20 16:43:58 +0100

    updated the api

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,4 @@
-// export const API_URL = 'https://techapi.nanocodes.com.ng';
-export const API_URL = 'https://api.onicsstore.com';
-export const CLIENT_ROOT_URL = 'http://localhost:3000';
+export const API_URL = 'https://techapi.nanocodes.com.ng';
+// export const API_URL = 'https://api.onicsstore.com';
+// export const CLIENT_ROOT_URL = 'http://localhost:3000';
+export const CLIENT_ROOT_URL = 'https://nanotech.nanocodes.com.ng';
